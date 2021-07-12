2edf355ed4757e371eaec4db9563e4e1f7db8a44
79479334 <79479334@qq.com>
2021-07-12 17:08:59 +0800

    2021-07-12

--- provider-user/src/main/java/com/microservice/provideruser/ProviderUserApplication.java
+++ provider-user/src/main/java/com/microservice/provideruser/ProviderUserApplication.java
@@ -2,8 +2,10 @@ package com.microservice.provideruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
+@EnableHystrix
 public class ProviderUserApplication {
 
     public static void main(String[] args) {
