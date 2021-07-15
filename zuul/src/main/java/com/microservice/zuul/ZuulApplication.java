e6ddf0fc126bc5b92249b22aa4fc897ad8aaa3a9
79479334 <79479334@qq.com>
2021-07-15 21:13:37 +0800

    2021-07-15

--- zuul/src/main/java/com/microservice/zuul/ZuulApplication.java
+++ zuul/src/main/java/com/microservice/zuul/ZuulApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 @EnableDiscoveryClient
 public class ZuulApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
     }
