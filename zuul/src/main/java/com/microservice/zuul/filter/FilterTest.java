bdb5e3612ac31ebe299678bc0087bdde595d9222
79479334 <79479334@qq.com>
2021-07-21 21:40:33 +0800

    2021-07-21

--- zuul/src/main/java/com/microservice/zuul/filter/FilterTest.java
+++ zuul/src/main/java/com/microservice/zuul/filter/FilterTest.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
  * @Description:
  */
 public class FilterTest extends ZuulFilter {
+    
     public String filterType() {
         //执行器类型：pre -> route -> post -> error
         return "pre";
